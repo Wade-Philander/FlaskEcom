a6aefae693bd2863d36e3c64bf284e16ffb250f6
Wade-Philander <wadephilander7@gmail.com>
2021-06-09 16:30:03 +0200

    EOD 09/06

---
+++ test/intergration/model_test.py
@@ -0,0 +1,27 @@
+from typing import NewType
+from test import BaseTest, db
+from market.models import Item, User
+
+
+class TestAllModel(BaseTest):
+    
+    #test that user can be saved and deleted
+    def test_user_crud(self):
+        with self.app_context():
+            new_user = User(data = 'New user')
+            #assert that this user user does not exist
+            results = db.session.query(User).filter_by(data='New user').first()
+            self.assertIsNone(results)
+            #save user to db
+            db.session.add(new_user)
+            db.session.commit()
+            #delete user from db
+            db.session.delete(new_user)
+    #test if item gets saved 
+    def test_item_crud(self):
+        with self.app_context():
+            new_item = Item(data = 'New Item')
+            result = db.session.query(Item).filter_by(data='New Item').first()
+            self.assertIsNone(result)
+            db.session.add(new_item)
+            db.session.commit()
--- test/intergration/models_test.py
+++
@@ -1,2 +0,0 @@
-from test import BaseTest
-from market.models import   
